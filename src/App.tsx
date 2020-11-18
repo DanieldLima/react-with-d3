2136fe2ea02957eca73533272fbc1cbbc456405f
Daniel Lima <familiasilva.daniel@gmail.com>
2020-11-17 22:29:35 -0300

    First commit - The basics video [01]

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
+import { select } from 'd3';
+
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([25, 30, 45, 60, 20])
+
+  const svgRef = useRef<SVGSVGElement>(null!);
+
+  useEffect(() => {
+    const { current: svgCurrentRef } = svgRef;
+
+    const svg = select(svgCurrentRef);
+    svg
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('r', value => value)
+      .attr('cx', value => (value * 2))
+      .attr('cy', value => (value * 2))
+      .attr('stroke', 'red')
+
+  }, [data])
+
+  console.log('data ->', data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <svg ref={svgRef} />
+      <br/>
+      <button onClick={() => {
+        setData((prevState) => prevState.map(value => (value + 5)))
+      }}>
+        Update data
+      </button>
+      <button onClick={() => {
+        setData((prevState) => prevState.filter(value => (value < 35)))
+      }}>
+        Filter data
+      </button>
+    </>
   );
 }
 
