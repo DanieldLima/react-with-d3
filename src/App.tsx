2dd8777652d48adef64f476f3616a9a99deae033
Daniel Lima <familiasilva.daniel@gmail.com>
2020-12-11 13:25:50 -0300

    Created Axes and Scales - video [03]

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React, {useEffect, useRef, useState} from 'react';
-import { select, line, curveCardinal } from 'd3';
+import React, { useEffect, useRef, useState } from 'react';
+import { select, line, curveCardinal, axisBottom, axisRight, scaleLinear} from 'd3';
 
 import './App.css';
 
 function App() {
-  const [data, setData] = useState<[number, number][]>([[0, 0], [50, 70], [100, 35], [150, 75], [200, 30], [250, 120], [300, 90]])
+  const [data, setData] = useState<[number, number][]>([[0, 0], [0, 70], [100, 35], [150, 75], [200, 30], [250, 120], [300, 90]])
 
   const svgRef = useRef<SVGSVGElement>(null!);
 
   useEffect(() => {
     const { current: svgCurrentRef } = svgRef;
-
     const svg = select(svgCurrentRef);
-    const pathLine = line().curve(curveCardinal.tension(0))
-      .x((value, idx) => value[0])
-      .y((value, idx) => 150 - value[1])
+
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((idx) => `${Number(idx) + 1}`)
+
+    svg.select('.x-axis')
+      .style('transform', 'translateY(150px)')
+      .call(xAxis as any)
+
+    svg.select('.y-axis')
+      .style('transform', 'translateX(300px)')
+      .call(axisRight(yScale) as any)
+
+    const pathLine = line().curve(curveCardinal)
+      .x((value, idx) => xScale(idx))
+      .y((value, idx) => yScale(value[1]))
 
     svg
-      .selectAll('path')
+      .selectAll('.line')
       .data([data])
       .join('path')
-      .attr('d', (value) => pathLine(value))
+      .attr('class', 'line')
+      .attr('d', pathLine)
       .attr('fill', 'none')
       .attr('stroke', 'blue')
 
@@ -30,7 +51,10 @@ function App() {
 
   return (
     <>
-      <svg ref={svgRef} />
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       <br/>
       <button onClick={() => {
         setData((prevState) =>
