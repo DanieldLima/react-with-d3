60e206ed0d9e020f83f8e09bb3b5f92e7830a64b
Daniel Lima <familiasilva.daniel@gmail.com>
2020-12-12 02:36:46 -0300

    Interactivity [5]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { select, scaleBand, axisBottom, axisRight, scaleLinear, scaleOrdinal } from 'd3';
+import { select, scaleBand, axisBottom, axisRight, scaleLinear, pointer } from 'd3';
 
 import './App.css';
 
+const value = [25, 30, 45, 60, 10, 65, 75];
+const months = ['01', '02', '03', '04', '05', '06', '07'];
+
 function App() {
-  const [data, setData] = useState<[number, number][]>([[0, 10], [0, 70], [100, 35], [150, 75], [200, 30], [250, 120], [300, 90]])
+  const [axisData, setAxisData] = useState(() => {
+    return value.map((value, idx) => ([months[idx], value]))
+  })
 
   const svgRef = useRef<SVGSVGElement>(null!);
 
@@ -12,9 +17,13 @@ function App() {
     const { current: svgCurrentRef } = svgRef;
     const svg = select(svgCurrentRef);
 
+    const domainXData = axisData.map((value) => value[0])
+
+    console.log('filter ->', axisData.map((value) => value[0]));
+
     const xScale = scaleBand()
-      .domain(data.map((_, idx) => String(idx)))
       .range([0, 300])
+      .domain(domainXData as string[])
       .padding(0.5);
 
     const yScale = scaleLinear()
@@ -28,7 +37,7 @@ function App() {
 
 
     const xAxis = axisBottom(xScale)
-      .ticks(data.length)
+      .ticks(axisData.length)
 
     svg.select('.x-axis')
       .style('transform', 'translateY(150px)')
@@ -40,21 +49,45 @@ function App() {
 
     svg
       .selectAll('.bar')
-      .data(data)
+      .data(axisData)
       .join('rect')
       .attr('class', 'bar')
       .style('transform', 'scale(1, -1)')
-      .attr('x', (value, idx) => String(xScale(`${idx}`)))
+      .attr('x', (value) => xScale(value[0] as string) as number)
       .attr('y', -150)
       .attr('width', xScale.bandwidth())
+      .on('mouseenter', (event, value) => {
+
+        // events have changed in d3 v6:
+        // https://observablehq.com/@d3/d3v6-migration-guide#events
+        // const index = svg.selectAll(".bar").nodes().indexOf(event.currentTarget);
+
+        svg
+          .selectAll('.tooltip')
+          .data([event])
+          .join('text')
+          .attr('class', 'tooltip')
+          .attr("text-anchor", "middle")
+          .text(value[1])
+          .attr('x', xScale(value[0] as string) as number + (xScale.bandwidth() / 2))
+          .style('transform', `translateY(${yScale(value[1] as number) - 6}px)`)
+          .attr('text-anchor', 'middle')
+          .transition()
+          .attr('opacity', 1)
+          .style('transform', `translateY(${yScale(value[1] as number) - 8}px)`);
+
+      })
+      .on('mouseleave', () => {
+        svg.select('.tooltip').remove()
+      })
       .transition()
-      .attr('fill', (value) => colorScale(value[1]))
-      .attr('height', (value) => 150 - yScale(value[1]))
+      .attr('fill', (value) => colorScale(value[1] as number))
+      .attr('height', (value) => 150 - yScale(value[1] as number))
 
 
-  }, [data])
+  }, [axisData])
 
-  console.log('data ->', data);
+  console.log('axisData ->', axisData);
 
   return (
     <>
@@ -64,18 +97,26 @@ function App() {
       </svg>
       <br/>
       <button onClick={() => {
-        setData((prevState) =>
-          prevState.map((value) =>
-            value.map(item => (item + 10))
-          ) as [number, number][]
-        )
+        setAxisData((prevState) =>
+          prevState.map((value) => [value[0], value[1] as number + 10]))
       }}>
         Update data
       </button>
       <button onClick={() => {
-        setData((prevState) => prevState.filter(value => (value.map(item => item < 35))))
+        setAxisData((prevState) => prevState.filter((value) => value[1] < 65))
       }}>
         Filter data
+      </button>
+      <button onClick={() => {
+        if (+axisData[axisData.length - 1][0] < 12) {
+          setAxisData((prevState) => {
+            const round = Math.round(Math.random() * 100)
+            const newValueMonth = +prevState[prevState.length - 1][0] + 1;
+            return [...prevState, [newValueMonth < 10 ? `0${newValueMonth}` : String(newValueMonth), round]]
+          })
+        }
+      }}>
+        Add data
       </button>
     </>
   );
